79874dbb112e27c2decf49f2f9260602a7604125
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-16 02:12:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ def get_average_age_for_season(data, season)
   counter = 0
   ageTotal = 0
   data[season].each {|elemObj|
-    ageTotal += elemObj["age"].to_i
+    ageTotal += elemObj["age"].to_f
     counter += 1
   }
   (ageTotal/counter).round
